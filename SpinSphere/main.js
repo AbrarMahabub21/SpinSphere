99be335e437f8099fd581ac6bb4d1fe87510edc9
Abrar Mahabub (Nowrid) <abrar.nowrid@gmail.com>
2024-08-24 10:17:10 +0600

    completed

--- SpinSphere/main.js
+++ SpinSphere/main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import gsap from "gsap";
 import "./style.css";
 //Scene
 const scene = new THREE.Scene();
@@ -38,10 +39,16 @@ scene.add(camera);
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(5);
 renderer.render(scene, camera);
 
 //controls
 const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 3;
 
 //resize
 window.addEventListener("resize", () => {
@@ -57,8 +64,44 @@ window.addEventListener("resize", () => {
 });
 
 const loop = () => {
+  controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(loop);
 };
 
 loop();
+
+//timeline gsap
+const t1 = gsap.timeline({ defaults: { duration: 1 } });
+t1.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+t1.fromTo("nav", { y: "-100%" }, { y: "0%" });
+t1.fromTo(".title", { opacity: 0 }, { opacity: 1 });
+t1.fromTo("footer", { opacity: 0 }, { opacity: 1 });
+
+//mouse animation
+let mouseDown = false;
+let rgb = [];
+window.addEventListener("mousedown", () => {
+  mouseDown = true;
+});
+window.addEventListener("mouseup", () => {
+  mouseDown = false;
+});
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round(e.pageX / sizes.width) * 255,
+      Math.round(e.pageY / sizes.height) * 255,
+      150,
+    ];
+    //set color
+    const newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+    new THREE.Color(`rgb(0,100,150)`);
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
